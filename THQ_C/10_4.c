612623208d5cd3e8866516d708eb4d061b082ed7
Your Name <lwei.lsoul@qq.com>
2016-11-18 01:38:05 +0800

    增加说明

--- THQ_C/10_4.c
+++ THQ_C/10_4.c
@@ -33,3 +33,12 @@ int main()
         return 0;
 
     }
+/*
+程序功能及要点回顾：
+功能：从键盘输入10个学生的有关数据，然后转存到磁盘文件中去
+要点：fread(buffer,size,count,fp);fwrite(buffer,size,count,fp)
+    buffer:表示一个地址，对fread来说是用来存放从文件读入的数据的存储区的地址；对于fwrite来说是要把此地址开始的存储区中的数据向文件输出(以上指的是起始地址)
+    size:要读写的字节数
+    count:要读写多少个数据项(每个数据项的长度size)
+    fp:FILE类型的指针
+*/
